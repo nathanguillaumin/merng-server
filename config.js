2cdd0c795ead63cf855c6540024fc667a1b53319
Nathan Guillaumin <nathan.guillaumin@outlook.com>
2021-01-15 14:40:29 +0100

    init server

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    "mongodb+srv://43P5gjKgMR1ZAl1R:43P5gjKgMR1ZAl1R@cluster0.fszvm.mongodb.net/merng?retryWrites=true&w=majority",
+  SECRET_KEY: "Some secret key",
+};
